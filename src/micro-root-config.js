1a977880384dcec5351c64859a20782e9c79b288
andrei misyurin <andrew.misiyrin@gmail.com>
2024-09-26 01:22:35 +0600

    init

---
+++ src/micro-root-config.js
@@ -0,0 +1,35 @@
+import { registerApplication, start } from "single-spa";
+
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
+registerApplication({
+  name: "@micro/react-app",
+  app: () => System.import("@micro/react-app"),
+  activeWhen: (location) => location.pathname.startsWith('/react-app'),
+});
+
+registerApplication({
+  name: "@micro/vue-cli",
+  app: () => System.import("@micro/vue-cli"),
+  activeWhen: (location) => location.pathname.startsWith('/vue-cli'),
+});
+
+registerApplication({
+  name: "@micro/vue-vite",
+  app: () => System.import('@micro/vue-vite'),
+  activeWhen: (location) => location.pathname.startsWith('/vite-vue'),
+});
+
+// add importmap
+(function () {
+  window.localStorage.setItem('devtools', 'true')
+})();
+
+start();
